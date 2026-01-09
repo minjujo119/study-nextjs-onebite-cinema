f0839a69bb2f250623fbb9f2db57d1304e588ea5
minjujo119 <minjujo119@gmail.com>
2026-01-08 21:07:48 -0500

    Day6 과제 제출: SSR 방식으로 데이터 fetch하기

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,46 +1,37 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import style from "./[id].module.css";
+import fetchMovieDescription from "@/lib/fetch-movie-description";
 
-const mockData = {
-  id: 5,
-  title: "위플래시",
-  releaseDate: "2025-03-12",
-  company: "(주)NEW",
-  genres: ["드라마"],
-  subTitle:
-    "“세상에서 제일 해로운 말이 뭔지 알아? ‘그 정도면 잘했어’야” 미친놈 vs 미친놈, 집념과 광기가 폭발한다!",
-  description:
-    "뉴욕의 명문 음악 학교에 다니지만 특별히 주목받지 못하던 드러머 앤드류. 최고의 드러머를 꿈꾸며 연습에만 매진하던 그는 어느 날, 우연히 교내 최고의 밴드를 이끄는 플레쳐 교수의 눈에 띄어 그의 밴드에 발탁된다. 그러나 모욕적인 폭언과 폭력을 휘두르며 완벽을 강요하는 플레쳐 교수의 무자비한 교수법으로 인해 앤드류는 능력을 증명해야 한다는 생각에 점차 미쳐가고, 앤드류가 광기에 휩싸일수록 플레쳐 교수의 완벽을 향한 집념 역시 높아지는데…! “세상에서 제일 해로운 말이 뭔지 알아? ‘그 정도면 잘했어’야” 미친놈 vs 미친놈, 집념과 광기가 폭발한다!",
-  runtime: 106,
-  posterImgUrl:
-    "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20250211_13%2F1739238451278AUJm0_JPEG%2Fmovie_image.jpg",
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const movie = await fetchMovieDescription(Number(id));
+  return {
+    props: { movie },
+  };
 };
 
-export default function Page() {
-  const {
-    // id,
-    title,
-    subTitle,
-    description,
-    releaseDate,
-    company,
-    genres,
-    runtime,
-    posterImgUrl,
-  } = mockData;
+export default function Page({
+  movie,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!movie) {
+    return "문제가 발생했습니다. 다시 시도해주세요.";
+  }
   return (
     <div>
       <div
         className={style.image_box}
-        style={{ backgroundImage: `url(${posterImgUrl})` }}
+        style={{ backgroundImage: `url(${movie.posterImgUrl})` }}
       >
-        <img src={posterImgUrl} alt={title} />
+        <img src={movie.posterImgUrl} alt={movie.title} />
       </div>
       <div className={style.text_box}>
-        <h1>{title}</h1>
-        <p>{`${releaseDate} / ${genres} / ${runtime}`}</p>
-        <p>{company}</p>
-        <h2>{subTitle}</h2>
-        <p>{description}</p>
+        <h1>{movie.title}</h1>
+        <p>{`${movie.releaseDate} / ${movie.genres} / ${movie.runtime}`}</p>
+        <p>{movie.company}</p>
+        <h2>{movie.subTitle}</h2>
+        <p>{movie.description}</p>
       </div>
     </div>
   );
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,24 @@
 import SearchLayout from "@/component/search-layout";
 import { ReactNode } from "react";
-import movies from "@/mock/dummy.json";
 import MovieItem from "@/component/movie-item";
 import style from "./index.module.css";
+import fetchAllMovies from "@/lib/fetch-all-movies";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const movies = await fetchAllMovies(q as string);
+
+  return {
+    props: { movies },
+  };
+};
+
+export default function Page({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.movie_list}>
       {movies.map((movie) => (
